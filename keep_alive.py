7354d1d72afb1883d2501c3ecf4009c78acd3a56
kvvde <realayamkw@gmail.com>
2021-07-02 16:23:29 +0700

    flask

--- keep_alive.py
+++ keep_alive.py
@@ -26,7 +26,7 @@ def home():
 
 def run():
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
 
 def keep_alive():
     t = Thread(target=run)
@@ -35,4 +35,4 @@ def keep_alive():
 if __name__ == '__main__':
     # Bind to PORT if defined, otherwise default to 5000.
     port = int(os.environ.get('PORT', 5000))
-    app.run(host='0.0.0.0', port=port, debug=True)
+    app.run(host='0.0.0.0', port=port)
